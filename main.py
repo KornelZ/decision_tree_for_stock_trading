14d5c80d6e8d29f3072a28086cd8dcbe6327b703
Aleksander Kuśmierczyk <Aleksander.Kusmierczyk@gmail.com>
2018-01-15 20:25:09 +0100

    console application added

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ COLUMNS = ["MACD_SELL", "MACD_BUY", "EMA(C)", "EMA(V)",
 TARGETS = ["BUY", "SELL", "NEUTRAL"]
 
 
-def main():
-    matrix = preprocess_data(DATA_PATH)
+def main(data_path, test_size):
+    matrix = preprocess_data(data_path)
     label_matrix(matrix)
     data_frame = pd.DataFrame(matrix, columns=COLUMNS)
-    df_train = data_frame.iloc[1500:, :]
-    df_test = data_frame.iloc[:1500, :]
+    df_train = data_frame.iloc[test_size:, :]
+    df_test = data_frame.iloc[:test_size, :]
     features = list(df_train.columns[:7])
     y = df_train["ACTION"]
     x = df_train[features]
@@ -23,10 +23,20 @@ def main():
     get_code(dt, features, TARGETS)
     y_test = df_test["ACTION"]
     x_test = df_test[features]
-    print(dt.score(x_test, y_test))
+    return dt.score(x_test, y_test)
 
+def main_loop():
+    end = False
+    while end == False:
+        path = input('please type in the dataset path:')
+        test_size = int(input('please type in the test dataset size'))
+        result = main(path,test_size)
+        print(result)
+        finish = input('if you want to quit press "q"')
+        if finish == "q":
+            end = True;
 
 if __name__ == "__main__":
-    main()
+    main_loop()
 
 
